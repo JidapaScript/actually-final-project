04bfecd3794bf598f92779bc42bd6927beba505d
JidapaScript <jidapasnidvongs@gmail.com>
2024-05-08 00:01:16 +0700

    components from old repository

---
+++ components/TopBar.jsx
@@ -0,0 +1,15 @@
+export default function TopBar(){
+    return (
+        <div className="flex-row justify-between fixed w-full mx-20 my-12">
+            <a href="/profile" className="profile-sm"> 
+            <img className="profile-img-sm" src="/profiles/profile_sm.png" alt="profile" /> 
+            </a>
+            <a href="/feed" className="actually-mainpage"> 
+            <img className="actually-logo" src="/logos/actually.svg" alt="actually" /> 
+            </a>
+            <a href="#" className="search-bar"> 
+            <img className="search" src="/icons/search.svg" alt="search" /> 
+            </a>
+        </div>
+    );
+}
